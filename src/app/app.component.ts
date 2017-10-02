9bfdb4d9bf6171060264da424dd76ce47a1a9773
井川 拓信 <h_igawa@ardito.jp>
2017-10-02 16:16:31 +0900

    「進捗どうですか？」画面を実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  /**
+   * 問い掛け
+   */
+  public question = 1;
+
+  ngOnInit(): void {
+    Observable
+      .interval(1000)
+      .subscribe(() => this.changeQuestion());
+  }
+
+  /**
+   * 問い掛けを変更する。
+   */
+  private changeQuestion(): void {
+    const getQ = () => Math.ceil(Math.random() * 4);
+
+    let question = getQ();
+    while (this.question === question) {
+      question = getQ();
+    }
+
+    this.question = question;
+  }
 }
